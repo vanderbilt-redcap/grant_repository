821c1a07fc8f86cd44a4d6c33999f40940ea4b2c
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-04-06 10:28:56 -0500

    r01 -> r

--- grantsHook.php
+++ grantsHook.php
@@ -4,31 +4,31 @@ $(document).ready(function() {
 		var val = $('[name=grants_number]').val();
 		if (val.match(/K\d\d/)) {
 			$('#k_awards-tr').show();
-			$('#r01_awards-tr').hide();
+			$('#r_awards-tr').hide();
 			$('#misc_awards-tr').hide();
 			$('#lrp_awards-tr').hide();
 			$('#va_merit_awards-tr').hide();
-		} else if (val.match(/R01/)) {
+		} else if (val.match(/R\d\d/)) {
 			$('#k_awards-tr').hide();
-			$('#r01_awards-tr').show();
+			$('#r_awards-tr').show();
 			$('#misc_awards-tr').hide();
 			$('#lrp_awards-tr').hide();
 			$('#va_merit_awards-tr').hide();
 		} else if (val.match(/LRP/)) {
 			$('#k_awards-tr').hide();
-			$('#r01_awards-tr').hide();
+			$('#r_awards-tr').hide();
 			$('#misc_awards-tr').hide();
 			$('#lrp_awards-tr').show();
 			$('#va_merit_awards-tr').hide();
 		} else if (val.match(/I01/)) {
 			$('#k_awards-tr').hide();
-			$('#r01_awards-tr').hide();
+			$('#r_awards-tr').hide();
 			$('#misc_awards-tr').hide();
 			$('#lrp_awards-tr').hide();
 			$('#va_merit_awards-tr').show();
 		} else if (val) {
 			$('#k_awards-tr').hide();
-			$('#r01_awards-tr').hide();
+			$('#r_awards-tr').hide();
 			$('#misc_awards-tr').show();
 			$('#lrp_awards-tr').hide();
 			$('#va_merit_awards-tr').hide();
