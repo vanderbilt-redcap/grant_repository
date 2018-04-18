38051665a68b36005ecee8e8bdf5fa74efe59997
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-04-18 16:34:22 -0500

    setup for third round

--- recover.php
+++ recover.php
@@ -119,7 +119,7 @@ foreach ($records as $record) {
 		$cntTotal++;
 	}
 	foreach ($edocs as $edocID) {
-		$sql = "UPDATE redcap_edocs_metadata SET delete_date = NULL WHERE doc_id = $edocID";
+		$sql = "UPDATE redcap_edocs_metadata SET delete_date = NULL, date_deleted_server = NULL WHERE doc_id = $edocID";
 		echo $sql."<br><br>";
 		db_query($sql);
 		echo db_error();
