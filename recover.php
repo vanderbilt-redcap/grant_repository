ae1cf2eb277c7e047631475aa6c32ad5b6baade1
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-04-18 12:19:30 -0500

    initial commit

--- recover.php
+++ recover.php
@@ -49,10 +49,10 @@ $records = array(
 );
 
 foreach ($records as $record) {
-	$sql = "SELECT sql_log FROM redcap_log_event WHERE pk = '".db_real_escape_string($record)."' AND project_id = 27635 AND event = 'UPDATE'";
+	$sql = "SELECT pk, sql_log FROM redcap_log_event WHERE pk = '".db_real_escape_string($record)."' AND project_id = 27635 AND event = 'UPDATE'";
 	$q = db_query($sql);
 	echo db_num_rows($q)."<br>";
 	while ($row = db_fetch_assoc($q)) {
-		// echo json_encode($row)."<br>";
+		echo json_encode($row)."<br>";
 	}
 }
