d05e43f236459ce1d3fb4377b52718ca6431fc4b
Scott Pearson <scott.j.pearson@vanderbilt.edu>
2018-04-19 10:28:09 -0500

    1

--- recover.php
+++ recover.php
@@ -66,7 +66,7 @@ foreach ($records3 as $record) {
 	} else {
 		$sign = "=";
 	}
-	$sql = "SELECT sql_log FROM redcap_log_event WHERE pk $sign '".db_real_escape_string($record)."' AND project_id = 27635 AND event = 'UPDATE' AND sql_log IS NOT NULL";
+	$sql = "SELECT sql_log FROM redcap_log_event WHERE pk $sign '".db_real_escape_string($record)."' AND project_id = 27635 AND (event = 'UPDATE' OR event='INSERT') AND sql_log IS NOT NULL";
 	$q = db_query($sql);
 	if (db_num_rows($q) === 0 || preg_match("/186 /", $record)) {
 		$sql = "SELECT sql_log FROM redcap_log_event WHERE pk $sign '".db_real_escape_string($record)."' AND project_id = 27635 AND sql_log IS NOT NULL";
