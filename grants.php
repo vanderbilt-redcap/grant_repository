bdefd6e6d2b08f22684cdc29dd4a39c8149b10b5
Scott J. Pearson <scott.j.pearson@vumc.org>
2020-09-01 15:16:44 -0500

    Undo Debug

--- grants.php
+++ grants.php
@@ -58,7 +58,6 @@ elseif ($sort == 'format') {
 	$sortSql = "ORDER BY d6.value";
 }
 
-$extra = "";
 $awardClause = "";
 foreach ($awards as $award => $awardTitle) {
 	if (isset($_GET[$award]) && $_GET[$award]) {
@@ -67,7 +66,6 @@ foreach ($awards as $award => $awardTitle) {
 			$awardField = $award;
 			$awardClause = "INNER JOIN redcap_data d7 ON (d7.project_id =d.project_id AND d7.record = d.record AND d7.field_name = '$awardField' AND d7.value IN ('".implode("','", array_keys($choices[$award]))."'))";
 			$search = "all ".$awardTitle;
-			$extra = ", DISTINCT d7.field_name as 'award_field'";
 		} else {
 			$awardField = $award;
 			$awardClause = "INNER JOIN redcap_data d7 ON (d7.project_id =d.project_id AND d7.record = d.record AND d7.field_name = '$awardField' AND d7.value='$awardValue')";
@@ -77,7 +75,7 @@ foreach ($awards as $award => $awardTitle) {
 }
 
 # Get the list of grants
-$sql = "SELECT d.record as 'record', d.value as 'title', d2.value as 'pi', d3.value as 'number', d4.value as 'file', d5.value as 'date', d6.value as 'format' $extra
+$sql = "SELECT d.record as 'record', d.value as 'title', d2.value as 'pi', d3.value as 'number', d4.value as 'file', d5.value as 'date', d6.value as 'format'
 		FROM redcap_data d
 		JOIN redcap_data d2
 		LEFT JOIN redcap_data d3 ON (d3.project_id =d.project_id AND d3.record = d.record AND d3.field_name = 'grants_number')
