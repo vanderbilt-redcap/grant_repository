1b513b7928a1ffb4349aa0103b59e6930cd1b25b
Scott J. Pearson <scott.j.pearson@vumc.org>
2020-09-16 17:08:28 -0500

    Undo Debug

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ $timestamp = date('Y-m-d');
 $role = "";
 $vunet = "";
 
-if ($_GET['uid']) {
-	$userid = $_GET['uid'];
-}
+# spoofing tool - provide uid in the GET params in the URL
+// if ($_GET['uid']) {
+	// $userid = $_GET['uid'];
+// }
 
 # query table to authenticate user
 $sql = "SELECT a.value as 'userid', a2.value as 'role'
