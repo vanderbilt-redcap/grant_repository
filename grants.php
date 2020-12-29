e4adba5d7e9c70dddb6865b7530165153c51b231
oriapp <VICTR.Server.Admins@vumc.org>
2020-12-29 16:12:32 -0600

    resolve multiple jquery references

--- grants.php
+++ grants.php
@@ -108,7 +108,7 @@ else
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
-		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 	</head>
 	<body>
@@ -198,7 +198,6 @@ echo "<input type='hidden' name='s' value='' />";
 echo "<input type='hidden' name='o' value='<?= $sort ?>' />";
 echo "</form>";
 ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 	function displayFilterButton() {
 		var item = $('#award_type').val();
