f0352c1f937bdf22f8d685c448fbf8d73268d396
Scott J. Pearson <scott.j.pearson@vumc.org>
2020-12-29 16:08:18 -0600

    Use DataTables

--- grants.php
+++ grants.php
@@ -107,8 +107,8 @@ else
 
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="css/basic.css">
 		<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 		<script src="//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 	</head>
 	<body>
