405fedec9eb1860a429742e3c49efdf4bcb318df
Scott J. Pearson <scott.j.pearson@vumc.org>
2022-10-04 16:34:16 -0500

    Update to LDAP DS

--- Application.php
+++ Application.php
@@ -10,4 +10,18 @@ class Application {
 			error_log($mssg);
 		}
 	}
+
+    public static function getCredentialsDir() {
+        $options = [
+            "/app001/credentials",
+            "/Users/pearsosj/credentials",
+            "/Users/scottjpearson/credentials",
+        ];
+        foreach ($options as $dir) {
+            if (file_exists($dir)) {
+                return $dir;
+            }
+        }
+        return "";
+    }
 }
