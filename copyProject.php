d68cf9c435c3fcbfabf9483c2377a6f8441b3b99
Scott J. Pearson <scott.j.pearson@vumc.org>
2023-01-23 17:16:38 -0600

    Save in old project - bug fix for file copy

--- copyProject.php
+++ copyProject.php
@@ -9,17 +9,22 @@ $destProject = 27635;
 $fileFields = ["grants_file", "email_file"];
 
 $redcapData = \REDCap::getData($srcProject, "json-array");
-
 $id = 1;
 foreach ($redcapData as $i => $row) {
-    foreach ($fileFields as $field) {
-        if ($row[$field]) {
-            $redcapData[$i][$field] = \REDCap::copyFile($row[$field], $destProject);
-        }
-    }
     $redcapData[$i]["record_id"] = $id;
     $id++;
 }
 
 $output = \REDCap::saveData($destProject, "json-array", $redcapData);
 echo json_encode($output);
+
+foreach ($redcapData as $i => $row) {
+    foreach ($fileFields as $field) {
+        if ($row[$field]) {
+            $origDocId = $row[$field];
+            $newDocId = \REDCap::copyFile($origDocId, $destProject);
+            \REDCap::addFileToField($newDocId, $destProject, $row['record_id'], $field);
+        }
+    }
+}
+
