6c855f9930097c7a07c245123274c3e8d4c85b18
moorejr5 <james.r.moore@vumc.org>
2025-09-10 10:31:48 -0500

    Updates for statistics page.

--- js/statistics.js
+++ js/statistics.js
@@ -1,3 +1,13 @@
 waitForElm('#stats_table').then((elm) => {
     ajaxHTMLToElement('statResults',{'searchParams':[]},elm.id)
 });
+
+function toggleDrilldown(target_id) {
+    let target = document.getElementById(target_id);
+
+    if (target.style.display === "none" || target.style.display === "") {
+        target.style.display = "block";
+    } else {
+        target.style.display = "none";
+    }
+}
